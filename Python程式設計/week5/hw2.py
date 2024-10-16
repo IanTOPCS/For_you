6ba4aa5f7d35fea5878c81e24c6d485cb492169a
Ian <IanLin6225@gmail.com>
2024-10-17 00:06:54 +0800

    DataStructure todo

--- Python程式設計/week5/hw2.py
+++ Python程式設計/week5/hw2.py
@@ -1,3 +1,22 @@
+import itertools
 c = input()
 n = ord(c.upper())-ord('A')+1
-sor = 
+sor=[]
+for i in range(n):
+    sor.append(chr(ord('A')+i))
+
+# print(sor)
+
+answer="{frozenset(p):i for i in range(1, n+1, 1) for p in itertools.combinations(sor, i)}"
+
+
+_cmd ='' 
+while True:
+    try:
+        _s = input()
+        _cmd += _s+'\n'
+    except EOFError:
+        break
+
+p = compile(_cmd,'default','exec')
+exec(p,globals())
