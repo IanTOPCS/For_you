cb110c22c052ce886525e0e0f7a72c47c325fda1
Ian <IanLin6225@gmail.com>
2024-11-28 19:07:13 +0800

    python week 11

--- Python程式設計/week11/find_diffday.py
+++ Python程式設計/week11/find_diffday.py
@@ -4,10 +4,6 @@ from datetime import datetime, timedelta
 def list_file_by_time(directory, d):
     today = datetime.today()
     for dirpath, dirnames, filenames in os.walk(directory):
-        dir_mtime = datetime.fromtimestamp(os.path.getmtime(dirpath))
-        time_diff = today - dir_mtime
-        if time_diff <= timedelta(days = d):
-            yield dirpath, dir_mtime
         for filename in filenames:
             full_file_name = os.path.join(dirpath, filename)
             file_mtime =  datetime.fromtimestamp(os.path.getmtime(full_file_name))
