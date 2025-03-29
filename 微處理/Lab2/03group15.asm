62399a3847f9e8c5a0fffcff56bcb7c2fd1646b8
Ian Lin <IanLin6225@gmail.com>
2025-03-29 15:44:34 +0800

    feat: micro-chip dealing

---
+++ 微處理/Lab2/03group15.asm
@@ -0,0 +1,65 @@
+start:
+sp0:
+	MOV P1, #0xc0
+	JNB P2.0, spF
+	JMP sp1
+sp1:
+	MOV P1, #0xf9
+	JNB P2.0, sp0
+	JMP sp2
+sp2:
+	MOV P1, #0xa4
+	JNB P2.0, sp1
+	JMP sp3
+sp3:	
+	MOV P1, #0xb0
+	JNB P2.0, sp2
+	JMP sp4
+sp4:
+	MOV P1, #0x99
+	JNB P2.0, sp3
+	JMP sp5
+sp5:
+	MOV P1, #0x92
+	JNB P2.0, sp4
+	JMP sp6
+sp6:
+	MOV P1, #0x82
+	JNB P2.0, sp5
+	JMP sp7
+sp7:
+	MOV P1, #0xd8
+	JNB P2.0, sp6
+	JMP sp8
+sp8:
+	MOV P1, #0x80
+	JNB P2.0, sp7
+	JMP sp9
+sp9:
+	MOV P1, #0x90
+	JNB P2.0, sp8
+	JMP spA
+spA:
+	MOV P1, #0x88
+	JNB P2.0, sp9
+	JMP spB
+spB:
+	MOV P1, #0x83
+	JNB P2.0, spA
+	JMP spC
+spC:
+	MOV P1, #0xc6
+	JNB P2.0, spB
+	JMP spD
+spD:
+	MOV P1, #0xa1
+	JNB P2.0, spC
+	JMP spE
+spE:
+	MOV P1, #0x86
+	JNB P2.0, spD
+	JMP spF
+spF:
+	MOV P1, #0x8E
+	JNB P2.0, spE
+	JMP sp0
---
+++ 微處理/Lab6/group15_lab6.asm
@@ -0,0 +1,82 @@
+	ORG	0h
+	JMP INIT
+
+INIT:
+	MOV	20H, #1		;陣列的起點
+	MOV	21H, #3		; 
+	MOV	22H, #6		
+	MOV	23H, #9		
+	MOV	24H, #13		
+	MOV	25H, #15		
+	MOV	26H, #18		;
+	MOV	27H, #22
+	MOV	28H, #25
+	MOV	29H, #28
+
+MAIN:               	;使用者主程式
+	MOV	R0, #20H		;陣列的起點
+	MOV	R1, #10	;陣列的長度
+	MOV 0X30, R0
+	MOV 0X31, R1
+	CALL	QUARTILE
+	JMP	$   		;跳回自己，無限迴圈, 在此檢?輸出
+QUARTILE:
+	CALL MEDIAN		;在此填入副程式
+	MOV AR2, R6
+	MOV AR3, R7
+
+	MOV A, 0X31
+	RRC A
+	MOV R1, A
+	
+	MOV A, 0X30
+	MOV R0, A
+
+	
+
+	CALL MEDIAN
+	MOV AR4, R6
+	MOV AR5, R7
+
+	MOV A, 0X31
+	CLR C
+	RRC A
+	MOV R1, A
+	
+	MOV A, 0X30
+	ADD A, 0X31
+	
+	SUBB A, R1
+	MOV R0, A
+
+	CALL MEDIAN
+	RET
+MEDIAN:
+	CLR C
+	MOV A, R1
+	RRC A
+	JC ODD  
+	JMP EVEN
+	ODD:
+		ADD A, R0
+		MOV R0, A
+		MOV AR6, @R0
+		MOV R7, #0H
+		RET
+	EVEN:
+		ADD A, R0
+		MOV R0, A
+		DEC R0
+		MOV A, @R0
+		INC R0
+		ADD A, @R0
+		RRC A
+		MOV R6, A
+		JC EBIN
+		MOV R7, #0
+		RET
+	EBIN:
+		MOV R7, #1
+		RET
+		
+	
